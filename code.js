ee6dac51ad9743a83ee03ea77dca75f6bfbe3cc0
CastleRun <786901a@gmail.com>
2017-05-16 14:52:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,43 @@
 var box = document.getElementById("outputBox");
 box.addText = function(text,endline){this.value+=text;if(endline){this.value+="\n";}};
 
+function loadDoc(fname){
+  var xhttp; //xhttp object
+  var fileObj;
+  if(window.XMLHttpRequest){
+    //modern browsers
+    xhttp = new XMLHttpRequest();
+  }else{
+    //ie6 and ie5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.onreadystatechange = function(){if(this.readyState==4&&this.status==200){fileObj=this.responseText;}};
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+  return fileObj;
+}
+
+doNothing(v){
+  return undefined;
+}
+
+function getFile(file){
+  var x;
+  try{
+    x = JSON.parse(getFile(file));
+  }
+  catch(err){
+    doNothing(err);
+    x = getFile(file);
+  }
+}
+
+function start(){
+  var ver = getFile("version.json");
+  box.addText(ver.name,true);
+  box.addText(ver.version,true);
+}
+
 function enter(text){
   box.addText(text, true);
 }
