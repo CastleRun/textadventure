60b07242afbf121ec383e197c9b5047b1a1cd83b
CastleRun <786901a@gmail.com>
2017-05-16 12:36:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-var n = document.getElementById("input")
+var box = document.getElementById("outputBox");
+box.addText = function(text,endline){this.value+=text;if(endline){this.value+="\n";}};
+
+function enter(text){
+  box.addText(text, true);
+}
+
+function Handle(e, text){
+  if(e.keyCode=13){
+    e.preventDefault();
+    
+    enter(text);
+  }
+  return false;
+}
